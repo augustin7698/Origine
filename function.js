ca22ae85cdbb2081f3e8fda8cebe90970b11a513
augustin jelocha <86960242+augustin7698@users.noreply.github.com>
2021-10-26 21:59:54 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,6 +1,50 @@
 //CONFIG
 console.log('Origine is connected');
 
+function config(jquery, react, vue, angular, D3, dojo, anime, AOS, chart, bootstrapp) {
+	if (jquery == 1) {
+		include("https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js");
+	}
+	if (react == 1) {
+		include("https://unpkg.com/react-dom@16/umd/react-dom.development.js");
+		include("https://unpkg.com/react@16/umd/react.development.js");
+	}
+
+	if (vue == 1) {
+		include("https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js");
+	}
+
+	if (angular == 1) {
+		include("https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js");
+	}
+
+	if (D3 == 1) {
+		include("https://d3js.org/d3.v7.min.js");
+	}
+
+	if (dojo == 1) {
+		include("https://ajax.googleapis.com/ajax/libs/dojo/1.14.1/dojo/dojo.js");
+	}
+
+	if (anime == 1) {
+		include("https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js");
+	}
+
+	if (AOS == 1) {
+		include("https://unpkg.com/aos@2.3.1/dist/aos.js");
+		document.getElementsByTagName('head')[0].innerHTML = document.getElementsByTagName('head')[0].innerHTML + "<link rel='stylesheet' type='text/css' href='https://unpkg.com/aos@2.3.1/dist/aos.css' />";
+	}
+
+	if (chart == 1) {
+		include("https://cdn.jsdelivr.net/npm/chart.js@3.5.1/dist/chart.min.js");
+	}
+
+	if (bootstrapp == 1) {
+		include("https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js");
+		document.getElementsByTagName('head')[0].innerHTML = document.getElementsByTagName('head')[0].innerHTML + "<link rel='stylesheet' type='text/css' href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' />";
+	}
+}
+
 //FUNCTION
 
 // USEFUL SIMPLE FUNCTION
@@ -18,6 +62,12 @@ function repeat(NumRepeat, FuncToExe, arg1, arg2, arg3, arg4, arg5, arg6, arg7, 
 	}
 }
 
+function interval(func, evry) {
+	setInterval(function() {
+		func
+	},evry);
+}
+
 function copy(ToCopy, Mess) {
 	navigator.clipboard.writeText(ToCopy).then(function() {
 		if (Mess != undefined) {
@@ -28,28 +78,57 @@ function copy(ToCopy, Mess) {
 	});
 }
 
-// MODIFY YOUR PAGE
-
-function add(Id, NewContent) {
-	content = document.getElementById(Id).innerHTML;
-	document.getElementById(Id).innerHTML = content + NewContent;
+function date() {
+	var heure = new Date();
+	return date = {
+		'year': heure.getFullYear(),
+		'month': heure.getMonth(),
+		'day': heure.getDay(),
+		'hour': heure.getHours(),
+		'minute': heure.getMinutes(),
+		'second': heure.getSeconds(),
+		'GMT': heure.toGMTString()
+	};
 }
 
-function del(Id) {
+function include(file) {
+	$.getScript(file);
+}
+
+// MODIFY YOUR PAGE
+
+function addhtml(Id, NewContent) {
+	document.getElementById(Id).innerHTML = document.getElementById(Id).innerHTML + NewContent;
+}
+
+function delhtml(Id) {
 	document.getElementById(Id).innerHTML = "<!--suppressed content-->";
 }
 
-function mod(Id, NewContent) {
+function modhtml(Id, NewContent) {
 	document.getElementById(Id).innerHTML = NewContent;
+}
+
+function stylehtml(style_css) {
+	document.getElementById('script').innerHTML = document.getElementById('script').innerHTML + "<style type='text/css'>" + style_css + "</style>";
 }
 
 // NUMERICS
 
-function number(NumMin_or_NumToRound, NumMax) {
-	if (Number.isInteger(Math.floor(NumMax))) {
-		return Math.floor(Math.random() * (NumMax - NumMin_or_NumToRound + 1));
+function randomnumber(NumMin, NumMax) {
+	return Math.floor(Math.random() * (NumMax - NumMin_or_NumToRound + 1));
+}
+
+function roundnumber(number) {
+	return Math.floor(number);
+}
+
+function isnumeral(number) {
+	num = "'" + number + "'";
+	if(Number.isInteger(Math.floor(num))) {
+		return true;
 	} else {
-		return Math.floor(NumMin_or_NumToRound);
+		return false;
 	}
 }
 
@@ -64,6 +143,10 @@ async function autoscroll(Ms, Px) {
 	if (scrolling != window.scrollY) {} else {
 		await wait(arguments[0], 'autoscroll', arguments[0], arguments[1]);
 	}
+}
+
+function howscroll() {
+	return {'X': window.scrollX, 'Y': window.scrollY};
 }
 
 function goto(at) {

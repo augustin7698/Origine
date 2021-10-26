1229290bad1ff97ad84dc8a6a8fbc1165ed4148f
augustin jelocha <86960242+augustin7698@users.noreply.github.com>
2021-10-26 22:39:37 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 //CONFIG
-console.log('Origine is connect');
+console.log('Origine is connected');
 
 function app(jquery, react, vue, angular, D3, dojo, anime, AOS, chart, bootstrapp) {
 	if (jquery == 1) {

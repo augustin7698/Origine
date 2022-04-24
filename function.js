4ca1f15361e6d68fa18c0cb8475606f81070d881
augustin j <86960242+augustin7698@users.noreply.github.com>
2022-04-24 21:23:37 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,32 +1,31 @@
-//CONFIGURATION
+// CONFIGURATION
 console.log('Origine is connected');
 
-//FUNCTION
-
-// USEFUL SIMPLE FUNCTION
-
 async function wait(ms, then) {
-	await new Promise(resolve => setTimeout(resolve, Ms));
+	await new Promise(resolve => setTimeout(resolve, ms));
 	eval(arguments[1]);
 }
 
-function copy(ToCopy, alertMessage) {
-	navigator.clipboard.writeText(ToCopy).then(function() {
-		if (alertMessage != undefined) {
-			alert(alertMessage);
-		}
-	}, function() {
-		console.log('Error: document is no focused');
+
+function copy(ToCopy) {
+	navigator.clipboard.writeText(ToCopy).then(azertyuiopqsfghjklmwxcvbn="success" , function() {
+		console.log('Error: document not focused');
 	});
 }
 
-function style(new_style_code) {
-	document.getElementsByTagName('head')[0].innerHTML = document.getElementsByTagName('head')[0].innerHTML + "<style type='text/css'>" + new_style_code + "</style>";
+
+function style(new_style) {
+	document.getElementsByTagName('head')[0].innerHTML = document.getElementsByTagName('head')[0].innerHTML + "<style type='text/css'>" + new_style + "</style>";
 }
+
 
 function get(element) {
 	if (element.includes("#")) {
-		return document.getElementById(element.replace("#", ""));
+		if (document.getElementById(element.replace("#", ""))) {
+			return document.getElementById(element.replace("#", ""));
+		} else {
+			return null;
+		}
 	} else if (element.includes(".")) {
 		if (document.getElementsByClassName(element.replace(".", "")).length == 0) {
 			return null;
@@ -42,65 +41,35 @@ function get(element) {
 	}
 }
 
-function del(element) {
-	finit = false;
-	if (element.search("#") == 0) {
-		element = element.replace('#', '');
-		document.getElementById(element).innerHTML = null;
-		finit = true;
-	}
-	if (element.search(/[.]/g) == 0 && finit == false) {
-		finit = true;
-		element = element.replace('.', '');
-		if (num != undefined) {
-			document.getElementsByClassName(element)[num].innerHTML = null;
-		} else {
-			num = 0;
-			while (document.getElementsByClassName(element)[num] != undefined) {
-				num = num + 1;
-			}
-			while (num != 0) {
-				num = num - 1;
-				document.getElementsByClassName(element)[num].innerHTML = null;
-			}
-		}
-	}
-	if (finit == false) {
-		if (num != undefined) {
-			document.getElementsByTagName(element)[num].innerHTML = null;
-		} else {
-			num = 0;
-			while (document.getElementsByTagName(element)[num] != undefined) {
-				num = num + 1;
-			}
-			while (num != 0) {
-				num = num - 1;
-				document.getElementsByTagName(element)[num].innerHTML = null;
-			}
-		}
-	}
+
+x = 0;
+y = "";
+while (! y.includes(String.fromCharCode(x))) {
+    y+= String.fromCharCode(x);
+    x++;
 }
 
+letter = ("012345678abcdefghijklmnopqrstuvwxyz;<=>?[\\]^_`@ABCDEFGHIJKLMNOPQRSTUVWXYZ" + y.replace("0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz", "")).split("");
 
-letter = ["0","1","2","3","4","5","6","7","8","9"," ","	","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","µ","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","&","ß","«","»","~","\"","#","'","{","(","[","-","|","`","_","\\","^","@","€",")","]","°","=","}","¨","^","£","$","¤","%","§","!","/",":",";",".","’","“","”","?",",","<",">","*","+","ª","º","¿","¡","ƒ","½","¼","±","¾","÷","¹","³","²","‰","↨","↑","↓","→","←","↔","►","◄","▲","▼","™","©","®","¶","☺","☻","♥","♦","♣","♠","•","○","♂","♀","♪","♫","☼","‡","†","×","—","–","α","Α","β","Β","γ","Γ","ε","ϵ","θ","ϑ","κ","ϰ","π","ϖ","ρ","ϱ","σ","ς","Σ","ϕ","φ","ω","Ω","℧","ϝ","Ϝ","Ѐ","Ё","Ђ","Ѓ","Є","Ѕ","І","Ї","Ј","Љ","Њ","א","ב","ג","ד","ɐ","ɑ","ɒ","ɓ","ɔ","ɕ","ɖ","ɗ","ɘ","ə","ɚ","⠀","⠁","⠂","⠃","⠄","⠅","⠆","⠇"," ","ᚁ","ᚂ","ᚃ","ᚄ","ᚅ","ᚆ","ᚇ","ᚈ","ᚉ","ᚊ","ᚋ","ᚌ","ᚍ","ᚎ","ᚏ","ᚐ","ᚑ","ᚒ","ᚓ","ᚔ","ᚕ","ᚖ","ᚗ","ᚘ","ᚙ","ᚚ","᚛","᚜","ᚠ","ᚡ","ᚢ","ᚣ","ᚤ","ᚥ","ᚦ","ᚧ","ᚨ","ᚩ","ᚪ","ᚫ","ก","ข","ฃ","ค","ฅ","ฆ","ง","จ","ฉ","ช","¼","½","¾","↉","⅓","⅔","⅕","⅖","⅗","⅘","⅙","⅚","⅛","⅜","⅝","⅞","⅟","⅐","⅑","⅒","Ⅰ","Ⅱ","Ⅲ","Ⅳ","Ⅴ","Ⅵ","Ⅶ","Ⅷ","Ⅸ","Ⅹ","Ⅺ","Ⅻ","Ⅼ","Ⅽ","Ⅾ","Ⅿ","ↀ","ↁ","ↂ","Ↄ","ↇ","ↈ","ⅰ","ⅱ","ⅲ","ⅳ","ⅴ","ⅵ","ⅶ","ⅷ","ⅸ","ⅹ","ⅺ","ⅻ","ⅼ","ⅽ","ⅾ","ⅿ","ↄ","ℂ","ℍ","ℕ","ℙ","ℚ","ℝ","ℤ","∅","ă","&","a","b","r","e","v","e",";","ą","&","a","o","g","o","n",";","˘","&","b","r","e","v","e",";","±","×","÷","•","∂","∏","∑","−","∗","√","∧","∨","∩","∪","∫","∬","∭","∮","∯","∰","∱","∲","∳","⊕","⊗","⋅","∀","∃","∇","∞","‖","⌈","⌉","⌊","⌋","〈","〉","⟨","⟩","≡","≤","⩽","≥","⩾","∈","∋","∝","∴","∼","≅","≈","⊂","⊃","⊆","⊇","∥","≠","≢","∉","⊄","∦","←","↑","→","↓","↔","↕","↖","↗","╭","╮","╯","╰","➔","➘","➙","➚","➛","➜","➝","➞","⤀","⤁","⤂","⤃","⤄","⤅","⤆","⤇","⤈","°","∠","⊥","ψ","₠","₡","₢","₣","₤","₥","₦","₧","₨","₩","₪","₫","€","₭","₮","₯","₰","₱","₲","₳","₴","₵","₶","₷","₸","₹","♩","♪","♫","♬","♭","♮","♯","☥","☦","☧","☨","☩","☪","☫","☬","☭","☮","☯","♰","♱","✝","✞","✟","☰","☱","☲","☳","☴","☵","☶","☷","䷀","䷁","䷂","䷃","䷄","䷅","䷆","☼","☽","☾","☿","♀","♁","♂","♃","♄","♅","♆","♇","♈","♉","♊","♋","♌","♍","♎","♏","♐","♑","♒","♓","♔","♕","♖","♗","♘","♙","♚","♛","♜","♝","♞","♟","♠","♣","♥","♦","♤","♧","♡","♢","⁡","⁢","⁣","⁪","⁫","⁬","⁭","⁮","⁯","␀","␁","␂","␃","␄","␅","␆","␇","␈","␉","␊","␋","␌","␍","␎","␏","␐","␑","␒","␓","␔","␕","␖","␗","␘","␙","␚","␛","␜","␝","␞","␟","␠","␡","␤","␥","␦","─","━","│","┃","┄","┅","┆","┇","┈","┉","┊","┋","┌","⎋","⇥","⇤","⇪","⇧","⌃","⌥","","⌘","␣","⏎","↩","⌫","⌦","﹖","⃝","⇱","↖","↸","⇲","↘","⇞","⇟","↑","⇡","↓","⇣","←","⇠","→","⇢","⌧","⇭","⌤","⏏","⌽","ò","ó","ô","õ","ö","à","á","â","ã","ä","å","æ","ç","è","é","ê","ë","ì","í","î","ï","ñ","œ","ù","ú","û","ü","ý","ÿ"];
 
 function hash(str) {
 	// convert string to array
 	str = str.split("");
 	// transform the string into number
 	for (var i = str.length - 1; i >= 0; i--) {
-		// encode according to the parity of a number
+		// encode => parity / unparity
 		if (i%2 == 0) {
 			str[i] = (letter.indexOf(str[i]) ** 2 * Math.LOG10E) / 5;
 		} else {
-			str[i] = (letter.indexOf(str[i]) ** 2 * Math.LN2) / 5;
+			str[i] = (letter.indexOf(str[i]) ** 4 * Math.LN2) / 10;
 		}
 		// encode according to the parity of a number
 		if (str[i]%2 == 0) {
 			str.concat(str[i] - str[i - 1]);
 		}
 	}
-	// encode according to the last number
+
+	// encode => last number
 	for (var i = str.length - 1; i >= 0; i--) {
 		if (i == 0) {
 			str[i] = str[i] + str[i];
@@ -109,22 +78,18 @@ function hash(str) {
 		}
 		
 	}
+
 	// sum all the members of the array
 	str = eval(String(str).replaceAll(",", "+").replaceAll(" ", "+").replaceAll("-", ""));
+
 	// remove dots
 	str = String(str).replace(".", "");
+
 	// put the number in base 36
 	str = Number(str).toString(36);
 	return str;
 }
 
-function random(min, max) {
-	number = Math.floor(Math.random() * max);
-	while (number < min) {
-		number = Math.floor(Math.random() * max);
-	}
-	return number;
-}
 
 function round(number, numberAfterComa) {
 	dizaine = 1;
@@ -136,16 +101,96 @@ function round(number, numberAfterComa) {
 }
 
 
-userActions = []
+function lDistance(str1, str2) {
+	str1 = str1.toLowerCase();
+	str2 = str2.toLowerCase();
+	const track = Array(str2.length + 1).fill(null).map(() =>
+	Array(str1.length + 1).fill(null));
+	for (let i = 0; i <= str1.length; i += 1) {
+		track[0][i] = i;
+	}
+	for (let j = 0; j <= str2.length; j += 1) {
+		track[j][0] = j;
+	}
+	for (let j = 1; j <= str2.length; j += 1) {
+		for (let i = 1; i <= str1.length; i += 1) {
+			const indicator = str1[i - 1] === str2[j - 1] ? 0 : 1;
+			track[j][i] = Math.min(
+				track[j][i - 1] + 1,
+				track[j - 1][i] + 1,
+				track[j - 1][i - 1] + indicator,
+			);
+		}
+	}
+	return (track[str2.length][str1.length]);
+}
+
+
+function log(str) {
+	console.debug(str);
+}
+
+
+userActions = [];
 window.addEventListener("click", function() { 
-	userActions.push(event)
+	userActions.push(event);
 }, false);
 
 window.addEventListener("keydown", function() { 
-	userActions.push(event)
+	userActions.push(event);
 }, false);
 
 window.addEventListener("select", function() { 
-	userActions.push(event)
+	userActions.push(event);
 }, false);
 
+
+function toBase(number, oldBase, newBase) {
+	number = String(Math.round(Number(number)));
+	oldBase = Math.round(Number(oldBase));
+	newBase = Math.round(Number(newBase));
+
+	if (newBase == 10) {
+		returner = 0;
+
+		for (i = 0; i < number.length; ++i) {
+			if (letter.indexOf(number[i]) > oldBase) {
+				console.log("false oldBase");
+				break;
+			} else {
+				returner *= oldBase;
+				returner += letter.indexOf(number[i]);
+			}
+		}
+		if (returner == 0) {
+			returner = null;
+		}
+	} else if (oldBase == 10) {
+
+		if (newBase > letter.length) {
+			returner = null;
+			console.log("there is only 65534 base");
+		} else if (newBase <= 1) {
+			returner = null;
+			console.log("this base don't exist");
+		} else {
+			a = []
+			while (number) {
+				a.splice(0, 0, letter[number % newBase])
+				number = parseInt(number / newBase);
+			}
+			returner = a.join("");
+		}
+	} else {
+
+		intermidiate = toBase(String(number), oldBase, 10);
+		returner = toBase(intermidiate, 10, newBase);
+	}
+
+
+	if (returner == null || returner == "") {
+		return undefined;
+	} else {
+		return String(returner);
+	}
+}
